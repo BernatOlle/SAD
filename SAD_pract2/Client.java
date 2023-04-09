f0bacedc733bf1d24593e1db2d27a52c0731a573
BernatOlle <90847284+BernatOlle@users.noreply.github.com>
2023-04-09 05:40:29 -0400

    Add files via upload

---
+++ SAD_pract2/Client.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Client {
+    static BufferedReader in;
+    public static void main(String[] args) {
+        MySocket sc = new MySocket("localhost",2345);
+        in = new BufferedReader(new InputStreamReader(System.in));
+        sc.write_line(args[0]);
+    
+        new Thread() {
+            public void run(){
+                String linia;
+                        try {
+                            while((linia = in.readLine())!= null){
+                                sc.write_line(linia);
+                            
+                                if(linia.matches("exit")){
+                                    sc.close();
+                                    break;   
+                                }
+                            }
+                            sc.close();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    
+            }
+        }.start();
+
+
+        new Thread() {
+            public void run(){
+                String linia;
+                    while((linia = sc.read_line()) != null){
+                        if(linia == "exit"){
+                            break;
+                        }
+                        System.out.println(linia);
+                    }
+                }
+            
+
+        }.start();
+    
+        
+    }
+}
+
