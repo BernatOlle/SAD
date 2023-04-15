6fefe3c9a9ab516f823dae115ad2c3d9527141a3
bernatOlle <bernatollef@gmail.com>
2023-04-16 01:15:56 +0200

    exemple

--- Practica_Final 2/Practica_Final/server.py
+++ Practica_Final 2/Practica_Final/server.py
@@ -9,37 +9,41 @@ port = 5555
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    s.bind((server, port)) #connectes el socket amb el servidor i el port
+    s.bind((server, port))  # connectes el socket amb el servidor i el port
 
 except socket.error as e:
     str(e)
 
-s.listen(2) #escolta fins que es connecten dos clients 
+s.listen(2)  # escolta fins que es connecten dos clients
 print("Waiting for a connection, Server Started")
 
-players = [Player(10,10,50,50,(255,0,0)), Player(10,10+100*6,50,50 ,(0,0,255))]
+players = [Player(10, 10, 50, 50, (255, 0, 0)),
+           Player(10, 10+100*6, 50, 50, (0, 0, 255))]
 # Player (posicio x inici, posicio y inici , weigth , length , color )
+
 
 def threaded_client(conn, player):
 
-    conn.send(pickle.dumps(players[player])) # envia una copia serialitzada del player 
+    # envia una copia serialitzada del player
+    conn.send(pickle.dumps(players[player]))
     reply = " "
     while True:
         try:
-            data = pickle.loads(conn.recv(2048)) #recibe datos del cliente cuando se mueve
-            players[player] = data 
+            # recibe datos del cliente cuando se mueve
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
-            if not data: 
+            if not data:
                 print("Disconnected")
                 break
             else:
-                if player == 1: 
+                if player == 1:
                     reply = players[0]
                 else:
                     reply = players[1]
-                #imprimeix la posicio nova que has rebuts i envia la teva posicio 
-                print("Recieved: ",data) 
-                print("Sending: ",reply) 
+                # imprimeix la posicio nova que has rebuts i envia la teva posicio
+                print("Recieved: ", data)
+                print("Sending: ", reply)
 
             conn.sendall(pickle.dumps(reply))
         except:
@@ -48,12 +52,13 @@ def threaded_client(conn, player):
     print("Lost connection")
     conn.close()
 
+
 currentPlayer = 0
 
 while True:
     conn, addr = s.accept()
-    #escolta 2 connexions i crea dos threads per agafar la dada rebuda, actualitzarla i envia la seva actual
+    # escolta 2 connexions i crea dos threads per agafar la dada rebuda, actualitzarla i envia la seva actual
     print("Connected to: ", addr)
-    
+
     start_new_thread(threaded_client, (conn, currentPlayer))
     currentPlayer += 1
