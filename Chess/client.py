6e482e6841ef29365e05b1ec6c5e64ed283dc108
bernatOlle <bernatollef@gmail.com>
2023-04-16 13:09:51 +0200

    chess

---
+++ Chess/client.py
@@ -0,0 +1,127 @@
+import pygame
+from network import Network
+import pickle
+pygame.font.init()
+#funcioa
+width = 800
+height = 800
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255,0,0)
+blue = (0,0,255)
+square_size = width/8
+win = pygame.display.set_mode((width, height))
+
+
+
+class Button:
+    def __init__(self, text, x, y, color):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 150
+        self.height = 100
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("comicsans", 40)
+        text = font.render(self.text, 1, (255,255,255))
+        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
+
+    def click(self, pos):
+        x1 = pos[0]
+        y1 = pos[1]
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
+
+
+def redrawWindow(win, game, p):
+    win.fill((128,128,128))
+
+    if not (game.connected()):
+        font = pygame.font.SysFont("comicsans", 80)
+        text = font.render("Waiting for Player...", 1, (255,0,0), True)
+        win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+    else:
+        for row in range(8):
+            for col in range(8):
+                x = col * square_size
+                y = row * square_size
+                if (col+row) % 2 == 0:
+                    pygame.draw.rect(win, black, (x, y, square_size, square_size))
+                else:
+                    pygame.draw.rect(win, white, (x, y, square_size, square_size))
+        
+        pieces_p1 = game.get_player_pieces(0)
+        pieces_p2 = game.get_player_pieces(1)
+        
+        for i in range(len(pieces_p1)):
+            if p == 0:
+                color1 = red
+                color2 = blue
+            else:
+                color1 = blue
+                color2 = red    
+            pygame.draw.rect(win,color1,(pieces_p1[i][0]*100+10,pieces_p1[i][1]*100 + 10,80,80))
+            pygame.draw.rect(win,color2,(pieces_p2[i][0]*100+10,pieces_p2[i][1]*100 + 10,80,80))
+                    
+    pygame.display.update()
+
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    n = Network()
+    player = int(n.getP())
+    print("You are player", player)
+    print(str(player))
+    pygame.display.set_caption("Client"+ str(player))
+    while run:
+        clock.tick(60)
+        try:
+            game = n.send("get")
+        except:
+            run = False
+            print("Couldn't get game")
+            break
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x,y = pygame.mouse.get_pos()
+                pos = [x//100,y//100]
+                n.send(str(pos))
+
+        redrawWindow(win, game, player)
+
+def menu_screen():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        win.fill((128, 128, 128))
+        font = pygame.font.SysFont("comicsans", 60)
+        text = font.render("Click to Play!", 1, (255,0,0))
+        win.blit(text, (100,200))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+
+    main()
+
+while True:
+    menu_screen()
---
+++ Chess/game.py
@@ -0,0 +1,88 @@
+class Game:
+    # te tota la info de la partida
+    def __init__(self, id):
+        self.turn = [True,False]
+        self.move = [False, False]
+        self.ready = False
+        self.id = id
+        self.pieces1 = []
+        self.pieces2 = []
+        self.all_pieces = self.put_pieces()
+        self.wins = [0, 0]
+        self.ties = 0
+        self.piece_sel = -1
+    
+    def put_pieces(self):
+        k=0
+        for y in range(2):
+            for x in range(8):
+                self.pieces1.append((x,y))
+                self.pieces2.append((x,y+6))
+                
+        return [self.pieces1,self.pieces2]
+
+    def get_player_pieces(self, p):
+        return self.all_pieces[p]
+    
+    def get_piece(self,p,move):
+        x,y = move.split(",")
+        move = [int(x[1]),int(y[1])]
+        print(move)
+        try:
+            print(self.all_pieces[p])
+            pos = self.all_pieces[p].index((int(x[1]),int(y[1])))
+        except:
+            pos = -1
+        return pos
+
+    def play(self, player, move):
+        print(self.turn)
+        if self.turn[player]== True:
+            pos = self.get_piece(player,move)
+            print(pos)
+            if pos != -1:
+                self.move[player] = True
+                self.piece_sel = pos
+                print("Selecionada figura pos:",pos)
+            elif self.move[player] == True:
+                x,y = move.split(",")
+                self.all_pieces[player][self.piece_sel] = (int(x[1]),int(y[1]))
+                self.move[player] = False
+                self.turn[player] = False
+                if player == 1:
+                    self.turn[0] = True
+                else:
+                    self.turn[1] = True
+                
+                
+
+    def connected(self):
+        return self.ready
+
+    def bothWent(self):
+        return self.p1Went and self.p2Went
+
+    def winner(self):
+
+        p1 = self.moves[0].upper()[0]
+        p2 = self.moves[1].upper()[0]
+
+        winner = -1
+        if p1 == "R" and p2 == "S":
+            winner = 0
+        elif p1 == "S" and p2 == "R":
+            winner = 1
+        elif p1 == "P" and p2 == "R":
+            winner = 0
+        elif p1 == "R" and p2 == "P":
+            winner = 1
+        elif p1 == "S" and p2 == "P":
+            winner = 0
+        elif p1 == "P" and p2 == "S":
+            winner = 1
+
+        return winner
+
+    def resetWent(self):
+        self.p1Went = False
+        self.p2Went = False
