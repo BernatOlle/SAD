d10a17d5982108d8cf336bdd5199250dc740d6d6
bernatOlle <bernatollef@gmail.com>
2023-04-18 10:11:12 +0200

    ip

--- Chess/network.py
+++ Chess/network.py
@@ -5,7 +5,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.192.225.34"
+        self.server = "172.20.10.7"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
